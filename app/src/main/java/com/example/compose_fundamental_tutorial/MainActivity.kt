fab776c53cb8360af16f290bf4adc3766272e61f
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-08-20 03:13:24 +0900

    Button 튜토리얼

--- app/src/main/java/com/example/compose_fundamental_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_fundamental_tutorial/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.compose_fundamental_tutorial
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -12,10 +18,7 @@ import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,10 +36,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import com.example.compose_fundamental_tutorial.ui.theme.Compose_fundamental_tutorialTheme
 import kotlin.math.cos
 import kotlin.math.sin
@@ -51,7 +51,7 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
 //                    Greeting("Android")
 //                    Container()
-                    ShapeContainer()
+                    ButtonsContainer()
                 }
             }
         }
@@ -192,6 +192,173 @@ fun VerticalContainer(){
         DummyBox()
         DummyBox()
     }
+}
+
+//onClick: () -> Unit,
+//modifier: Modifier = Modifier,
+//enabled: Boolean = true,
+//interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+//elevation: ButtonElevation? = ButtonDefaults.elevation(),
+//shape: Shape = MaterialTheme.shapes.small,
+//border: BorderStroke? = null,
+//colors: ButtonColors = ButtonDefaults.buttonColors(),
+//contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+//content: @Composable RowScope.() -> Unit
+
+// Button
+// enable: 클릭여부 처리
+// interactionSource: 사용자의 인터렉션 처리
+// elevation: 그림자 즉 버튼을 위로 띄우면서 그림자 그리기
+// 커스텀 그림자 넣는 법
+// shape: 모양
+// border: 테두리
+// colors: 버튼 색
+// contentPadding: 내용물 밀어넣는 공간
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ButtonsContainer(){
+
+    val buttonBorderGradient = Brush.horizontalGradient(listOf(Color.Yellow, Color.Red))
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val pressStatusTitle = if (isPressed) "버튼을 누르고 있다." else "버튼에서 손을 뗐다."
+
+    val interactionSourceForSecondBtn = remember { MutableInteractionSource() }
+
+    val isPressedForSecondBtn by interactionSourceForSecondBtn.collectIsPressedAsState()
+
+    val pressedBtnRadius = if (isPressedForSecondBtn) 0.dp else 20.dp
+
+    val pressedBtnRadiusWithAnim: Dp by animateDpAsState(
+        if (isPressedForSecondBtn) 0.dp else 20.dp
+    )
+
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp
+            ),
+            enabled = true,
+            onClick = {
+            Log.d("TAG", "ButtonsContainer: 버튼 1 클릭")
+        }) {
+            Text(text = "버튼 1")
+        }
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            enabled = true,
+            onClick = {
+                Log.d("TAG", "ButtonsContainer: 버튼 2 클릭")
+            }) {
+            Text(text = "버튼 2")
+        }
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            enabled = true,
+            shape = CircleShape,
+            onClick = {
+                Log.d("TAG", "ButtonsContainer: 버튼 3 클릭")
+            }) {
+            Text(text = "버튼 3")
+        }
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            enabled = true,
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(4.dp, Color.Red),
+            contentPadding = PaddingValues(top = 20.dp, bottom = 10.dp, start = 20.dp, end = 20.dp),
+            onClick = {
+                Log.d("TAG", "ButtonsContainer: 버튼 4 클릭")
+            }) {
+            Text(text = "버튼 4")
+        }
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            enabled = true,
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(4.dp, buttonBorderGradient),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Black,
+                disabledBackgroundColor = Color.LightGray
+            ),
+            interactionSource = interactionSource,
+            onClick = {
+                Log.d("TAG", "ButtonsContainer: 버튼 5 클릭")
+            }) {
+            Text(text = "버튼 5", color = Color.White)
+        }
+
+//        if (isPressed) {
+//            Text(text = "버튼을 누르고 있다.")
+//        } else {
+//            Text(text = "버튼에서 손을 뗐다.")
+//        }
+
+        Text(text = "$pressStatusTitle")
+
+        Button(
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            enabled = true,
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(4.dp, buttonBorderGradient),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Black,
+                disabledBackgroundColor = Color.LightGray
+            ),
+            interactionSource = interactionSourceForSecondBtn,
+            modifier = Modifier.drawColoredShadow(
+                color = Color.Blue,
+                alpha = 0.5f,
+                borderRadius = 10.dp,
+                shadowRadius = pressedBtnRadiusWithAnim,
+                offsetY = 0.dp,
+                offsetX = 0.dp,
+            ),
+            onClick = {
+                Log.d("TAG", "ButtonsContainer: 버튼 5 클릭")
+            }) {
+            Text(text = "버튼 5", color = Color.White)
+        }
+
+//        color: Color,
+//        alpha: Float = 0.2f,
+//        borderRadius: Dp = 0.dp,
+//        shadowRadius: Dp = 20.dp,
+//        offsetY: Dp = 0.dp,
+//        offsetX: Dp = 0.dp
+
+    }
+
 }
 
 //text: String,
@@ -428,7 +595,7 @@ fun Path.polygon(sides: Int, radius: Float, center: Offset) {
 @Composable
 fun DefaultPreview() {
     Compose_fundamental_tutorialTheme {
-        ShapeContainer()
+        ButtonsContainer()
 //        Container()
 //        Greeting("Android")
     }

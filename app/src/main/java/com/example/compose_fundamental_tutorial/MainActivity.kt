87de36236c0f26f7f39cc0b7afca5925b34a2ac3
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-11-01 00:36:54 +0900

    네비게이션 설정

--- app/src/main/java/com/example/compose_fundamental_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_fundamental_tutorial/MainActivity.kt
@@ -47,6 +47,10 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.compose_fundamental_tutorial.ui.theme.Compose_fundamental_tutorialTheme
 import kotlinx.coroutines.launch
 import kotlin.math.cos
@@ -65,12 +69,178 @@ class MainActivity : ComponentActivity() {
 //                    Container()
 //                    CheckBoxContainer()
 //                    MySnackbar()
-                    TextFieldTest()
+//                    TextFieldTest()
+                    NavigationGraph()
                 }
             }
         }
     }
 }
+
+// 네비게이션 라우트 이넘 (값을 가지는 이넘)
+enum class NAV_ROUTE(val routeName: String, val description: String, val btnColor: Color){
+    MAIN("MAIN", "메인 화면", Color(0xFF3949AB)),
+    LOGIN("LOGIN", "로그인 화면", Color(0xFF5E35B1)),
+    REGISTER("REGISTER", "회원가입 화면", Color(0xFFD81B60)),
+    USER_PROFILE("USER_PROFILE", "유저 프로필 화면", Color(0xFF00897B)),
+    SETTING("SETTING", "설정 화면", Color(0xFFF4511E))
+}
+
+// 네비게이션 라우트 액션
+class RouteAction(navHostController: NavHostController){
+
+    // 특정 라우트로 이동
+    val navTo: (NAV_ROUTE) -> Unit = { route ->
+        navHostController.navigate(route.routeName)
+    }
+
+    // 뒤로가기 이동
+    val goBack: () -> Unit = {
+        navHostController.navigateUp()
+    }
+}
+
+@Composable
+fun NavigationGraph(startRoute: NAV_ROUTE = NAV_ROUTE.MAIN) {
+
+    // 네비게이션 컨트롤러
+    val navController = rememberNavController()
+
+    // 네비게이션 라우트 액션
+    val routeAction = remember(navController) { RouteAction(navController) }
+
+    // NavHost 로 네비게이션 결정
+    // 네비게이션 연결할 녀석들을 설정한다
+    NavHost(navController, startRoute.routeName) {
+
+        // 라우트 이름 = 화면의 키
+        composable(NAV_ROUTE.MAIN.routeName){
+            // 화면 = 값
+            MainScreen(routeAction = routeAction)
+        }
+        // 라우트 이름 = 화면의 키
+        composable(NAV_ROUTE.LOGIN.routeName){
+            // 화면 = 값
+            LoginScreen(routeAction = routeAction)
+        }
+        // 라우트 이름 = 화면의 키
+        composable(NAV_ROUTE.REGISTER.routeName){
+            // 화면 = 값
+            RegisterScreen(routeAction = routeAction)
+        }
+        // 라우트 이름 = 화면의 키
+        composable(NAV_ROUTE.USER_PROFILE.routeName){
+            // 화면 = 값
+            UserProfileScreen(routeAction = routeAction)
+        }
+        // 라우트 이름 = 화면의 키
+        composable(NAV_ROUTE.SETTING.routeName){
+            // 화면 = 값
+            SettingScreen(routeAction = routeAction)
+        }
+    }
+
+}
+
+// 메인 화면
+@Composable
+fun MainScreen(routeAction: RouteAction){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(Modifier.padding(16.dp)) {
+            NavButton(route = NAV_ROUTE.LOGIN, routeAction = routeAction)
+            NavButton(route = NAV_ROUTE.REGISTER, routeAction = routeAction)
+            NavButton(route = NAV_ROUTE.USER_PROFILE, routeAction = routeAction)
+            NavButton(route = NAV_ROUTE.SETTING, routeAction = routeAction)
+        }
+    }
+}
+
+// 로그인 화면
+@Composable
+fun LoginScreen(routeAction: RouteAction){
+    Surface(Modifier.fillMaxSize()) {
+        Box(Modifier.padding(8.dp), Alignment.Center){
+            Text(text = "로그인 화면", style = TextStyle(Color.Black, 22.sp, FontWeight.Medium))
+            // 뒤로가기 버튼
+            Button(onClick = routeAction.goBack,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .offset(y = 100.dp)) {
+                Text("뒤로가기")
+            }
+        }
+    }
+}
+
+// 회원가입 화면
+@Composable
+fun RegisterScreen(routeAction: RouteAction){
+    Surface(Modifier.fillMaxSize()) {
+        Box(Modifier.padding(8.dp), Alignment.Center){
+            Text(text = "회원가입 화면", style = TextStyle(Color.Black, 22.sp, FontWeight.Medium))
+            // 뒤로가기 버튼
+            Button(onClick = routeAction.goBack,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .offset(y = 100.dp)) {
+                Text("뒤로가기")
+            }
+        }
+    }
+}
+
+// 유저 프로필 화면
+@Composable
+fun UserProfileScreen(routeAction: RouteAction){
+    Surface(Modifier.fillMaxSize()) {
+        Box(Modifier.padding(8.dp), Alignment.Center){
+            Text(text = "유저 프로필 화면", style = TextStyle(Color.Black, 22.sp, FontWeight.Medium))
+            // 뒤로가기 버튼
+            Button(onClick = routeAction.goBack,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .offset(y = 100.dp)) {
+                Text("뒤로가기")
+            }
+        }
+    }
+}
+
+// 설정 화면
+@Composable
+fun SettingScreen(routeAction: RouteAction){
+    Surface(Modifier.fillMaxSize()) {
+        Box(Modifier.padding(8.dp), Alignment.Center){
+            Text(text = "설정 화면", style = TextStyle(Color.Black, 22.sp, FontWeight.Medium))
+            // 뒤로가기 버튼
+            Button(onClick = routeAction.goBack,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .offset(y = 100.dp)) {
+                Text("뒤로가기")
+            }
+        }
+    }
+}
+
+// 콜럼에 있는 네비게이션 버튼
+@Composable
+fun ColumnScope.NavButton(route: NAV_ROUTE, routeAction: RouteAction){
+    Button(onClick = {
+        routeAction.navTo(route)
+    },colors = ButtonDefaults.buttonColors(backgroundColor = route.btnColor),
+        modifier = Modifier
+            .weight(1f)
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = route.description,
+        style = TextStyle(Color.White, 22.sp, FontWeight.Medium)
+        )
+    }
+}
+
+
 
 //fun TextField(
 //    value: TextFieldValue,
